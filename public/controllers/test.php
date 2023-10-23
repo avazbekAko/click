6a861d80558bcbc16efb4010829d8ed6ffabc0f4
Avazbek <eshmurodov.avazbek@mail.ru>
2023-10-24 01:29:33 +0300

    Start

--- public/controllers/test.php
+++ public/controllers/test.php
@@ -1,9 +1,6 @@
 <?php
 echo "Привет Авазбек";
 
-git init
-git add .
-git commit -m "Start"
-git branch -M main
-git remote add origin https://github.com/avazbekAko/click.git
-git push -u origin main
+// git add .
+// git commit -m "Start"
+// git push -u origin main
